df738154849299236d18084fecc59456b7538d63
Andre Wasem <46170060+ALWasem@users.noreply.github.com>
2026-06-10 15:56:04 -0500

    Add: moving-average-from-data-stream - submission-5

---
+++ Data Structures & Algorithms/moving-average-from-data-stream/submission-5.java
@@ -0,0 +1,34 @@
+class MovingAverage {
+
+    int size;
+    Queue<Integer> queue;
+
+    public MovingAverage(int size) {
+
+        this.size = size;
+        this.queue = new LinkedList<>();
+        
+    }
+    
+    public double next(int val) {
+        
+        queue.offer(val);
+
+        if(queue.size() > this.size)
+            queue.poll();
+
+        int total = 0;
+
+        for(Integer num : queue)
+            total += num;
+
+        return (double) total / queue.size();
+
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
