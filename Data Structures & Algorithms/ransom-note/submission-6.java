9326ba4362ecac397f056936b762b3008c8a8621
Andre Wasem <46170060+ALWasem@users.noreply.github.com>
2026-06-17 19:27:30 -0500

    Add: ransom-note - submission-6

---
+++ Data Structures & Algorithms/ransom-note/submission-6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        HashMap<Character, Integer> freq = new HashMap<>();
+
+        for(Character c : magazine.toCharArray()){
+            if(freq.containsKey(c))
+                freq.put(c, freq.get(c) + 1);
+            else
+                freq.put(c, 1);
+        }
+
+        for(Character c : ransomNote.toCharArray()){
+            if(freq.containsKey(c) && freq.get(c) > 0)
+                freq.put(c, freq.get(c) - 1);
+            else
+                return false;
+        }
+
+        return true;
+        
+    }
+}
